4204b0371452ba332c51ec3186c5ccdbffe1b8f9
Theo <theo.lacanaria@gmail.com>
2025-10-05 14:36:51 +0800

    implement getting a single tour

--- app.js
+++ app.js
@@ -22,6 +22,31 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+//adding a route handler for getting one tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  //req.params gives the parameters from the url
+  console.log(req.params);
+
+  //searching for the specific tour with matching ID
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
 //adding a post request handler
 app.post('/api/v1/tours', (req, res) => {
   //   console.log(req.body);
