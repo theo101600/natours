7b2a451c346e2c01066656fcbf57f9c0d01905b8
Theo <theo.lacanaria@gmail.com>
2025-10-11 12:45:53 +0800

    implement a small script for uploading dev data to database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/tourModel");
+
+dotenv.config({
+  path: "./config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connection successful!");
+  });
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`),
+  "utf-8",
+);
+
+// Import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data is successfully loaded");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete all data from Collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data deleted successfully");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// process.argv shows the arguments/commands inputted in the terminal
+console.log(process.argv);
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
