72d083c226ca24fa7a67b4a23a43a336c1f2e031
Theo <theo.lacanaria@gmail.com>
2025-10-12 14:02:13 +0800

    implement getMonthlyPlan using aggregate

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController");
 
 const router = express.Router();
@@ -19,6 +20,8 @@ const router = express.Router();
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 
 router.route("/tour-stats").get(getTourStats);
+
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
