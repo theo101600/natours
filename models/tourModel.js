df0dcb7624713b2cfcde0529f6b3b3a4573c707c
Theo <theo.lacanaria@gmail.com>
2025-10-12 15:03:23 +0800

    implement presave document middleware to add slug property

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-//writing a schema type options
+const slugify = require("slugify");
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -71,6 +73,17 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// Document Middleware: runs before .save() and .create()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 //making a model out of the schema
 const Tour = mongoose.model("Tour", tourSchema);
