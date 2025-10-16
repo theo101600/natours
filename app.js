38d5e45189b3000093b6fcc25511b5da05f813fa
Theo <theo.lacanaria@gmail.com>
2025-10-16 21:23:53 +0800

    implement basic error handling

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
-//1) MIDDLEWARES
-// Only running this morgan middleware(logging) when we are in development
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-//use((<request>, <response>, <next>) => {})
-app.use((req, res, next) => {
-  console.log('hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-//reading the contents of the tours-simple.json file
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-//2) ROUTE HANDLERS
+// Placing the code below the other route definition because that means the request did not hit the above routes
+// .all() means all the http method (get, post, put...)
+app.all("*", (req, res, next) => {
+  //Creating an error
+  const err = new Error(`Cannot find ${req.originalUrl} on this server.`);
+  err.status = "fail";
+  err.statusCode = 404;
 
-//3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+  // if the next() receives an argument, it automatically knows that an error occured
+  next(err);
+});
 
-//4) START SERVER
-//.listen(<port>, <callback function>)
+// by specifying four arguments, express automatically recognizes it as an error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
