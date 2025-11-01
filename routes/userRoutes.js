88986324e0f2252011a4298a8b46151331cad984
Theo <theo.lacanaria@gmail.com>
2025-11-01 12:50:03 +0800

    implement updating the password of the current user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,8 @@ const {
   login,
   forgotPassword,
   resetPassword,
+  updatePassword,
+  protect,
 } = require("../controllers/authController");
 
 const router = express.Router();
@@ -22,6 +24,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
+router.patch("/updateMyPassword", protect, updatePassword);
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
