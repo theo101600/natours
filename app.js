474d86f1b05ffc1e962c5909d4cd8cc5c1c9e2b7
Theo <theo.lacanaria@gmail.com>
2025-11-02 09:08:02 +0800

    implement data sanitization, prevent parameter polution

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,27 +16,61 @@ const userRouter = require("./routes/userRoutes");
 console.log(process.env.NODE_ENV);
 
 // Global Middlewares
+// set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour",
 });
-
 app.use("/api", limiter);
 
-app.use(express.json());
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    // allow certain parameters to have duplicates
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
+
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
   next();
 });
 
+// Routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
