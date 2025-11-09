3f05a2583e8902b791a05fa729771a8f8a18bcf6
Theo <theo.lacanaria@gmail.com>
2025-11-09 14:00:40 +0800

    set up pug, and view routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const path = require("path");
 const app = express();
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -13,11 +14,19 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
+const { restrictTo } = require("./controllers/authController");
 
 console.log(process.env.NODE_ENV);
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 // Global Middlewares
 // set Security HTTP headers
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(helmet());
 
 // Development logging
@@ -61,9 +70,6 @@ app.use(
   }),
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -72,6 +78,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
