e1f962199b8e57e97b3b0ce0b9d1b5d7a83a1977
Theo <theo.lacanaria@gmail.com>
2025-11-15 15:35:49 +0800

    implement account page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -43,3 +43,9 @@ exports.getLoginForm = catchAsync(async (req, res, next) => {
     title: "Log into your account",
   });
 });
+
+exports.getAccount = (req, res) => {
+  res.status(200).render("account", {
+    title: "Your account",
+  });
+};
