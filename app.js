f19e9ee0801680ddc1af595dd68e1006853ced07
Theo <theo.lacanaria@gmail.com>
2025-11-30 10:36:06 +0800

    implement CORS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -25,7 +26,10 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // Global Middlewares
-// set Security HTTP headers
+// Implement CORS
+app.use(cors());
+app.options("*", cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
